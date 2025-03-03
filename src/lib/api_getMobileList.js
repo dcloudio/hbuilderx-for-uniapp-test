633c523bcf6468418915e60e727d8757eb8b5b2d
wan201809 <53882753+wan201809@users.noreply.github.com>
2025-03-03 15:00:50 +0800

    优化 api_getMobileList.js 使用hx.extensions.getExtension替代hx.app.getMobileList获取设备列表

--- src/lib/api_getMobileList.js
+++ src/lib/api_getMobileList.js
@@ -10,6 +10,8 @@ const osName = os.platform();
 // 测试设备
 global.global_devicesList = {};
 
+var extension_launcher = undefined;
+
 /**
  * @description 通过Api hx.app.getMobileList, 获取当前电脑连接的手机设备
  * @description {Sting} testPlatform [ios|android|all]
@@ -20,27 +22,7 @@ let PLATFORM_ANDROID_SIMULATOR = 0x00000002;
 let PLATFORM_IOS = 0x00000004;
 let PLATFORM_IOS_SIMULATOR = 0x00000008;
 let PLATFORM_ALL = 0x00000010;
-async function api_getMobileList(testPlatform, isRefresh="N") {
-    hx.window.setStatusBarMessage("hbuilderx-for-uniapp-test: 正在获取测试设备列表...", 5000, 'info');
-
-    // console.log("============", testPlatform, global_devicesList)
-    if (isRefresh == "N") {
-        if (testPlatform == "all" &&
-            global_devicesList["ios_simulator"] != undefined &&
-            global_devicesList["android"] != undefined) {
-            return global_devicesList;
-        };
-        if (testPlatform == "harmony" && global_devicesList["harmony"] != undefined) {
-            return global_devicesList;
-        };
-    };
-
-    if (testPlatform == "harmony" && isRefresh == "Y") {
-        let h_tmp = await getHarmonyDeivcesListFormCmd();
-        global_devicesList["harmony"] = h_tmp;
-        return global_devicesList;
-    };
-
+async function getMobileList(testPlatform, isRefresh="N") {
     let deviceType = PLATFORM_ALL;
     if (testPlatform == 'ios') {
         deviceType = PLATFORM_IOS_SIMULATOR;
@@ -55,16 +37,9 @@ async function api_getMobileList(testPlatform, isRefresh="N") {
     let platform = {
         "platform": deviceType
     };
-
     let data = await hx.app.getMobileList(platform).then(data => {
         return data;
     });
-    // console.log("[api_getMobileList] -->", data);
-
-    // let data = await hx.app.getDevices().then(data => {
-    //     return data;
-    // });
-    // console.log("[api_getMobileList] -->", data);
 
     try{
         // 先这样，后期在区分修改
@@ -88,6 +63,91 @@ async function api_getMobileList(testPlatform, isRefresh="N") {
         global_devicesList = data;
         return data;
     }
+};
+
+/**
+ * @param {String} testPlatform
+ *
+ */
+async function getDevicesFormLauncher(testPlatform, isRefresh) {
+    // {
+    //     "iOS-iPhone": iphoneLauncher,
+    //     "android": androidLauncher,
+    //     "IOS_SIMULATOR": iossimLauncher,
+    //     "app-harmony": harmonyLauncher,
+    //     "mp-harmony": harmonyASLauncher,
+    // }
+    if (extension_launcher == undefined) {
+        extension_launcher = hx.extensions.getExtension("launcher");;
+    };
+    if (extension_launcher == undefined) {
+        return global_devicesList;
+    };
+    if (testPlatform == "all" || testPlatform == 'ios') {
+        let ios_simulator_list = await extension_launcher.getDevices({ platform:'IOS_SIMULATOR'});
+        if (ios_simulator_list && ios_simulator_list.length > 0){
+            global_devicesList["ios_simulator"] = ios_simulator_list;
+        };
+    };
+    if (testPlatform == "all" || testPlatform == 'android') {
+        let _android_list = await extension_launcher.getDevices({ platform:'android'});
+        if (_android_list && _android_list.length > 0){
+            global_devicesList["android"] = _android_list;
+        };
+    };
+    if (testPlatform == "all" || testPlatform == 'harmony') {
+        let _harmony_list = await extension_launcher.getDevices({ platform:'app-harmony'});
+        if (_harmony_list && _harmony_list.length > 0){
+            global_devicesList["harmony"] = _harmony_list;
+        };
+    };
+    return global_devicesList;
+};
+
+
+async function api_getMobileList(testPlatform, isRefresh="N") {
+    hx.window.setStatusBarMessage("hbuilderx-for-uniapp-test: 正在获取测试设备列表...", 5000, 'info');
+    // console.log("============", testPlatform, global_devicesList)
+
+    if (isRefresh == "N") {
+        if (testPlatform == "all" &&
+            global_devicesList["ios_simulator"] != undefined &&
+            global_devicesList["android"] != undefined) {
+            return global_devicesList;
+        };
+        if (testPlatform == "ios" && global_devicesList["ios_simulator"] != undefined) {
+            return global_devicesList;
+        };
+        if (testPlatform == "android" && global_devicesList["android"] != undefined) {
+            return global_devicesList;
+        };
+        if (testPlatform == "harmony" && global_devicesList["harmony"] != undefined) {
+            return global_devicesList;
+        };
+    };
+
+    if (testPlatform == "harmony" && isRefresh == "Y") {
+        let h_tmp = await getHarmonyDeivcesListFormCmd();
+        global_devicesList["harmony"] = h_tmp;
+        return global_devicesList;
+    };
+
+    let result = {};
+    let is_error = false;
+    try {
+        result = await getDevicesFormLauncher(testPlatform, isRefresh);
+    } catch (error) {
+        console.error(error);
+        is_error = true
+    };
+    // console.log("--->", result, is_error);
+
+    if (is_error) {
+        try {
+            result = await getMobileList(testPlatform, isRefresh);
+        } catch (error) {}
+    };
+    return result;
 };
 
 module.exports = api_getMobileList;
