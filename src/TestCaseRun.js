8eca86c0692926cbe0bf83c070bdfbf0537ab4d0
wan201809 <53882753+wan201809@users.noreply.github.com>
2025-08-13 18:24:52 +0800

    调整 因hx 4.75之后版本废弃API hx.unicloud.getExistsUnicloudAndBindSpace, 改用调用uniCloud插件

--- src/TestCaseRun.js
+++ src/TestCaseRun.js
@@ -176,6 +176,35 @@ class Common {
         };
     };
 
+    // 调用unicloud插件的getWorkspaceBindServiceSpace方法
+    async unicloud_extension_getWorkspaceBindServiceSpace(workspaceFolder) {
+        let u_tmp = [];
+        let u_cloud = await hx.extensions.getExtension("unicloud");
+        let u_result = await u_cloud.getWorkspaceBindServiceSpace({"workspaceFolder": workspaceFolder});
+        let u_allSpaces = u_result.allSpaces;
+        if (u_allSpaces == undefined || u_allSpaces.length == 0) {
+            return u_tmp;
+        };
+        for (let s of u_allSpaces) {
+            if (s.provider == "tcb") {
+                u_tmp.push({"id": s.spaceId,"name":s.spaceName,"provider":"tcb"});
+            };
+            if (s.provider == "aliyun") {
+                let t_ali = Object.assign({"id": s.spaceId, "name":s.spaceName}, s);
+                u_tmp.push(t_ali);
+            };
+            if (s.provider == "alipay") {
+                let t_a = Object.assign({"id": s.spaceId}, s);
+                u_tmp.push(t_a);
+            };
+            if (s.provider == "dcloud") {
+                let t_dcloud= Object.assign({"id": s.spaceId}, s);
+                u_tmp.push(t_dcloud);
+            };
+        };
+        return u_tmp;
+    };
+
     /**
      * @description 获取unicloud服务信息
      * @param {String} projectPath - 项目路径
@@ -196,11 +225,28 @@ class Common {
         };
         if (!workspaceFolder) { return };
 
-        let uniCloud_info = await hx.unicloud.getExistsUnicloudAndBindSpace({"workspaceFolder": workspaceFolder});
-        let allSpaces = uniCloud_info.allSpaces;
-        if (allSpaces && allSpaces.length > 0) {
-            unicloud_spaces_info = allSpaces
+        let is_error = false;
+        try {
+            // 老方法：只在4.75版本以及之前版本生效
+            let uniCloud_info = await hx.unicloud.getExistsUnicloudAndBindSpace({"workspaceFolder": workspaceFolder});
+            let allSpaces = uniCloud_info.allSpaces;
+            if (allSpaces && allSpaces.length > 0) {
+                unicloud_spaces_info = allSpaces
+            };
+        } catch (error) {
+            console.error("[自动化测试] 执行hx.unicloud.getExistsUnicloudAndBindSpace异常", error);
+            is_error = true;
         };
+
+        if (is_error) {
+            try {
+                console.error("[自动化测试] 开始执行 unicloud_extension_getWorkspaceBindServiceSpace");
+                unicloud_spaces_info = await this.unicloud_extension_getWorkspaceBindServiceSpace(workspaceFolder);
+                // console.error("[自动化测试] unicloud_spaces_info = ", unicloud_spaces_info);
+            } catch (error) {
+                console.error("[自动化测试] 通过unicloud.getWorkspaceBindServiceSpace获取服务空间信息异常。",  error);
+            };
+        }
     };
 
     /**
@@ -476,6 +522,7 @@ class RunTest extends Common {
             cmdOpts.env.UNIAPPX_KOTLIN_COMPILER_MEMORY = hx_config__uniappx_kotlin_compiler_memory;
         };
 
+        // console.error("unicloud_spaces_info =>", JSON.stringify(unicloud_spaces_info));
         if (unicloud_spaces_info.length > 0) {
             cmdOpts.env.UNI_CLOUD_SPACES = JSON.stringify(unicloud_spaces_info);
         };
