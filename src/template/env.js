1d31a428f8fff686027185e12fc64bf42432ed15
wan201809 <53882753+wan201809@users.noreply.github.com>
2025-12-03 15:30:40 +0800

    update src/template/env.js 补充webview配置项

--- src/template/env.js
+++ src/template/env.js
@@ -2,7 +2,12 @@
 // 备注：UNI_TEST_CUSTOM_ENV 用于自动化测试向uniapp-cli-vite编译器中传递一些环境变量，设置格式：key: value
 module.exports = {
     "is-custom-runtime": false,
-    "UNI_TEST_CUSTOM_ENV": {},
+    "UNI_TEST_CUSTOM_ENV": {
+        // 以下3个配置项用于定义以App-WebView方式运行的H5页面地址，方便自动化测试App-WebView场景
+        // "UNI_AUTOMATOR_APP_WEBVIEW": "true",
+        // "UNI_WEB_SERVICE_URL": "http://xxx.com/xxx.html",
+        // "UNI_AUTOMATOR_APP_WEBVIEW_SRC": "http://xxx.com/xxx.html"
+    },
     "compile": true,
     "h5": {
         "options": {
