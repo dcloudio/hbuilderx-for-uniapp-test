5fcf9be2fc070718c1583deb1889548ea8a6874d
wan <wdl@dcloud.io>
2026-06-12 11:17:33 +0800

    feat: src/utils/check_web_lib.js 解决特定包在linux运行测试到web的问题

--- src/utils/check_web_lib.js
+++ src/utils/check_web_lib.js
@@ -1,5 +1,9 @@
+const hx = require('hbuilderx');
 const fs = require('fs');
 const path = require('path');
+const os = require("os");
+
+const osName = os.platform();
 
 const browserMap = {
     chrome: 'chromium',
@@ -8,6 +12,21 @@ const browserMap = {
 };
 
 function checkWebLib(browser, nodeLibPath) {
+
+    // 针对linux ai包的特殊处理
+    const ms_playwright_dir = path.join(hx.env.appRoot, "plugins", "hbuilderx-for-uniapp-test-lib", "ms-playwright");
+    if (osName == "linux" && browser == "chrome" && fs.existsSync(ms_playwright_dir)) {
+        let ms_playwright_chrome = "";
+        if (fs.existsSync(ms_playwright_dir)) {
+            const chromium_dir = fs.readdirSync(ms_playwright_dir).find((item) => item.startsWith("chromium-"));
+            const chrome_linux_dir = chromium_dir ? fs.readdirSync(path.join(ms_playwright_dir, chromium_dir)).find((item) => item.startsWith("chrome-linux")) : "";
+            ms_playwright_chrome = chrome_linux_dir ? path.join(ms_playwright_dir, chromium_dir, chrome_linux_dir, "chrome") : "";
+        };
+        if (ms_playwright_chrome && fs.existsSync(ms_playwright_chrome)) {
+            return { "exists": true,  "browser": browser}
+        };
+    };
+
     const browserName = String(browser || '').toLowerCase();
     const browserTypeName = browserMap[browserName];
     const result = {
@@ -45,5 +64,5 @@ module.exports = {
     checkWebLib
 };
 
-let result = checkWebLib("chrome", "/Users/hx/DCloud/dcloud_test/env_run_lib/0.0.4/node_modules")
-console.log(result)
+// let result = checkWebLib("chrome", "/Users/hx/DCloud/dcloud_test/env_run_lib/0.0.4/node_modules")
+// console.log(result)
